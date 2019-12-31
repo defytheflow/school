13a7827ae438b186dc8014caf148b392230b2864
Artyom Danilov <wxamba162@gmail.com>
2019-12-31 14:32:38 +0300

    Made major improvements to the checking process of the <num> argument, refactored the script into functions and added figlet | lolcat use.

--- lesson.sh
+++ lesson.sh
@@ -3,29 +3,82 @@
 # Generates a default directory structure for a lesson.
 #
 # Usage:
-# 	lesson <num>
+#     lesson <num>
 
-RED="\033[1;31m"
-GREEN="\033[0;32m"
-YELLOW="\033[1;33m"
-NO_COLOR="\033[0m"
+# Variable Definitions
 
-# Check if a <num> argument was given
-if [[ $# -ne 1 ]]; then
-	printf "${YELLOW}Usage:${NO_COLOR} lesson <num>\n"
+MAX_LESSON_NUM=100
+LESSONS_DIR="Lessons"
+
+# Function Definitions
+
+# Prompt user if to proceed or not. 
+proceed() {
+	echo -n "Proceed? [y/n]: "
+	read ANS
+	if [[ ! "$ANS" =~ ^[yY][eE]?[sS]?$ ]]; then
+			exit 0
+	fi
+}
+
+# Returns text highlighted with white background
+reversed() {
+	echo "\033[7m$1\033[0m"
+}
+
+# Returns text in green color
+green() {
+	echo "\033[0;32m$1\033[0m"
+}
+
+# Reports an error in red color and exits the program
+error() {
+	printf "\033[1;31mError:\033[0m $1\n"
 	exit 1
+}
+
+# Reports a warning in yellow
+warning() {
+	printf "\033[1;33mWarning:\033[0m $1\n"
+}
+
+# Main part of the Script
+
+# Check if <num> argument was given
+if [[ $# -ne 1 ]]; then
+	error "Missing <num> argument"
 fi
 
-# Check if a <num> argument is an integer
-if [[ !  "$1" =~ ^[0-9]+$ ]]; then
-	printf "${RED}Error:${NO_COLOR} <num> argument must be an integer\n"
-	exit 1
+# Check if <num> argument is an integer
+if [[ ! "$1" =~ ^[0-9]+$ ]]; then
+	error "<num> argument must be an integer"
+fi
+
+# Check if <num> argument is less than or equal MAX_LESSON_NUM
+if [[ ! "$1" -le $MAX_LESSON_NUM ]]; then
+	warning "$1 exceed the MAX_LESSON_NUM"
+	proceed
 fi
 
 # Check if there is already a Lesson with number <num>
 if [[ -d $1 ]]; then
-	printf "${RED}Error:${NO_COLOR} Lesson $1 already exists\n"
+	error "Lesson number $(reversed $1) already exists"
 	exit 1
+fi
+
+# Check if inside of LESSONS_DIR directory
+if [[ ! "$(pwd)" =~ .+/$LESSONS_DIR$ ]]; then
+	warning "Not inside $(reversed $LESSONS_DIR) directory"
+	proceed
+fi
+# Check if figlet program is installed
+if [[ ! -x "$(command -v figlet)" ]]; then
+	error "$(reversed "figlet") is not installed. Please install it and run again."
+fi
+
+# Check if lolcat program is installed
+if [[ ! -x "$(command -v lolcat)" ]]; then
+	error "$(reversed "lolcat") is not installed. Please install it and run again."
 fi
 
 # Create the Lesson layout
@@ -37,4 +90,4 @@ mkdir $1/Whiteboard
 mkdir $1/Programs
 mkdir $1/Homework
 
-printf "${GREEN}Done!${NO_COLOR} Let's learn now!\n"
+figlet "Lesson $1" | lolcat
