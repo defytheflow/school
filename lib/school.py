ba85382a1f527580a565ba441c0f10208aab2b5d
Artyom Danilov <defytheflow@gmail.com>
2020-02-11 14:36:47 +0300

    'lib/school.py' - added more functionality to 'get_int' and 'get_float' functions, implemented 'error' function.

--- lib/school.py
+++ lib/school.py
@@ -5,50 +5,72 @@
 import sys
 
 
-def get_int(prompt: str="") -> int:
+def get_int(prompt: str="", force: bool=False) -> int:
     """
         Get a line of text from stdin and try to convert it to an int.
 
         Args:
             prompt (str): the prompt to display
+            force (bool): If True, prompts user until gets a valid int value
 
         Returns:
             int:  if user input can be converted to int
-            None: EOF or user input can not be converted to int
+            None: EOF or user input can not be converted to int (force=False)
 
         Exits on SIGINT
     """
-    try:
-        num = int(input(prompt))
-    except (ValueError, EOFError):
-        return None
-    except KeyboardInterrupt:
-        sys.exit("")
-    else:
-        return num
+    while True:
+        try:
+            i = int(input(prompt))
+        except ValueError:
+            if force:
+                continue
+            else:
+                return None
+        except EOFError:
+            if force:
+                print()
+                continue
+            else:
+                return None
+        except KeyboardInterrupt:
+            sys.exit("")
+        else:
+            return i
 
 
-def get_float(prompt: str="") -> float:
+def get_float(prompt: str="", force: bool=False) -> float:
     """
         Get a line of text from stdin and try to convert it to a float.
 
         Args:
             prompt (str): the prompt to display
+            force (bool): If True, prompts user until gets a valid float value
 
         Returns:
             float:  if user input can be converted to float
-            None:   EOF or user input can not be converted to float
+            None:   EOF or user input can not be converted to float (force=False)
 
         Exits on SIGINT
     """
-    try:
-        num = float(input(prompt))
-    except (ValueError, EOFError):
-        return None
-    except KeyboardInterrupt:
-        sys.exit("")
-    else:
-        return num
+    while True:
+        try:
+            f = float(input(prompt))
+        except ValueError:
+            if force:
+                continue
+            else:
+                return None
+        except EOFError:
+            if force:
+                print()
+                continue
+            else:
+                return None
+        except KeyboardInterrupt:
+            sys.exit("")
+        else:
+            return f
 
 
 def get_string(prompt: str="") -> str:
@@ -65,10 +87,23 @@ def get_string(prompt: str="") -> str:
         Exits on SIGINT
     """
     try:
-        string = input(prompt)
+        s = input(prompt)
     except EOFError:
         return None
     except KeyboardInterrupt:
         sys.exit("")
     else:
-        return string
+        return s
+
+
+def error(message: str="", fatal: bool=False):
+    """
+        Prints an error message to stderr.
+
+        Args:
+            message (str): the message to print
+            fatal  (bool): if True, terminates the program with error code 1.
+    """
+    print("Error:", message, file=sys.stderr)
+    if fatal:
+        sys.exit(1)
