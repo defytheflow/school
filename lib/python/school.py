c21a9ba9e44c205e0d2c96e355ce5b447dcd672f
Artyom Danilov <defytheflow@gmail.com>
2020-02-28 14:01:13 +0300

    'lib/python/' - merged '{new_school,school,utils}.py into one module 'school.py'

--- lib/python/school.py
+++ lib/python/school.py
@@ -6,10 +6,11 @@ import io
 import sys
 import readline
 
-from typing import TextIO
+from typing import TextIO, Callable, Union
 
 
-def get_int(prompt: str="", force: bool=False, stream: TextIO=sys.stdin) -> int:
+def get_int(prompt: str = "", force: bool = False,
+            stream: TextIO = sys.stdin) -> int:
     """
         Get a line of text from 'stream' and try to convert it to an int.
         If 'force' is True, prompts user until gets a valid int value.
@@ -48,7 +49,8 @@ def get_int(prompt: str="", force: bool=False, stream: TextIO=sys.stdin) -> int:
             return i
 
 
-def get_float(prompt: str="", force: bool=False, stream: TextIO=sys.stdin) -> float:
+def get_float(prompt: str = "", force: bool = False,
+              stream: TextIO = sys.stdin) -> float:
     """
         Get a line of text from 'stream' and try to convert it to a float.
         If 'force' is True, prompts user until gets a valid float value.
@@ -87,7 +89,7 @@ def get_float(prompt: str="", force: bool=False, stream: TextIO=sys.stdin) -> fl
             return f
 
 
-def get_string(prompt: str="", stream: TextIO=sys.stdin) -> str:
+def get_string(prompt: str = "", stream: TextIO = sys.stdin) -> str:
     """
         Get a line of text from 'stream'.
 
@@ -126,7 +128,75 @@ def get_string(prompt: str="", stream: TextIO=sys.stdin) -> str:
             sys.stdin = stdin
 
 
-def error(message: str="", status: int=1, fatal: bool=False):
+def get_input(prompt: str = "", type_: Callable = str, force: bool = False,
+              stream: TextIO = sys.stdin) -> Union[str, int, float]:
+    """
+
+        Get a line of text from 'stream' and try to convert it to
+        some datatype using 'type_' callable.
+
+        If 'force' is True, prompts user until gets a convertable value.
+        Optional 'stream' paramater can be set to read input from a file.
+
+        Args:
+          prompt (str)      : prompt to display (default - "").
+          type_  (Callable) : callable used to convert the return value to
+                              required datatype (default - str).
+          force  (bool)     : toggles force mode (default - False).
+          stream (TextIO)   : file stream to read from (default - sys.stdin).
+
+        Returns:
+          str  : 'type_' is 'str'
+          int  : 'type_' is 'int' and input can be converted to int.
+          float: 'type_' is 'float' and input can be converted to float.
+          None :  input can not be converted with 'type_' or reached EOF.
+
+        Exits on SIGINT
+
+    """
+    # Check 'type_' argument
+    if not callable(type_):
+        raise ValueError("'get_input' - Invalid type of 'type_' argument.")
+
+    # Check 'stream' argument
+    if not isinstance(stream, io.TextIOWrapper):
+        raise ValueError("'get_input' - Invalid type of 'stream' argument.")
+
+    stdin = sys.stdin  # original reference to sys.stdin
+
+    # if not reading from stdin, do not display the prompt
+    if stream != stdin:
+        prompt = None
+        sys.stdin = stream  # input() reads from stdin
+
+    while True:
+        if prompt is not None:
+            print(prompt, end="")
+        try:
+            input_ = type_(input())
+        except (ValueError, TypeError):
+            if force:
+                continue
+            return None
+        except EOFError:
+            print()
+            if force:
+                continue
+            return None
+        except KeyboardInterrupt:
+            sys.exit("")
+        else:
+            return input_
+        finally:
+            sys.stdin = stdin
+
+
+def print_err(*args, **kwargs):
+    """ A wrapper around built-in 'print' function connected to stderr. """
+    print(*args, **kwargs, file=sys.stderr)
+
+
+def error(message: str = "", status: int = 1, fatal: bool = False):
     """
         Prints an error message prepended with "Error: " to stderr.
         If 'fatal' is True, terminates the program with exit status 'status'.
--- lib/python/utils.py
+++
@@ -1,9 +0,0 @@
-""" Some utility functions. """
-
-import sys
-
-
-def print_err(*args, **kwargs):
-    """ A wrapper around built-in 'print' function connected to stderr. """
-    print(*args, **kwargs, file=sys.stderr)
-
