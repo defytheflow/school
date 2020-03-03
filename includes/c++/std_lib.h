08a56051724e7dc1038f23b2c1a1415a6023685a
Artyom Danilov <defytheflow@gmail.com>
2020-03-03 18:48:23 +0300

    'lib/python/school.py' - moved this module to new repository 'pylib'. 'std_lib.h' - just added some includes and a simple function for errors.

--- includes/c++/std_lib.h
+++ includes/c++/std_lib.h
@@ -1,7 +1,32 @@
+#include <cmath>
+#include <cstdlib>
+
 #include <string>
 #include <vector>
-#include <cmath>
-#include <iostream>
+
 #include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <stdexcept>
 
 using namespace std;
+
+inline void simple_error(const string& s)
+/* Write "Error: 's'" and exit the program. */
+{
+    cerr << "Error: " << s << '\n';
+    exit(1);
+}
+
+inline void error(const string& s)
+/* Just throws an exception. */
+{
+    throw runtime_error(s);
+}
+
+inline void error(const string&s, const string& s2)
+{
+    throw runtime_error(s + s2);
+}
